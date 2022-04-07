23bbdb38ee5697cd4c7128d5154f1dc37290f876
0SYmedia <103207557+0SYmedia@users.noreply.github.com>
2022-04-07 17:34:39 +0100

    Add files via upload

---
+++ global.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    document.getElementById("navBar").style.backgroundColor = "#0a0a11";
+    document.getElementById("navBar").style.boxShadow = "0 0 30px #000000";
+  } else {
+    document.getElementById("navBar").style.backgroundColor = "#0a0a11";
+    document.getElementById("navBar").style.boxShadow = "0 0 30px #00000000";
+  }
+}
+
+console.log("💗 I'm not sure what you're doing in the console but I hope you're having a nice day! ^_^")
